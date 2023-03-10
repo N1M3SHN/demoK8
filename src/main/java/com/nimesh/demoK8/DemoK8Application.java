3fc2f46c40904c14ff93dc4f587a4a1d5eb72dd8
nimesh.n <nimeshnelanga@outlook.com>
2023-03-09 22:02:34 -0500

    basic springboot app with jib

---
+++ src/main/java/com/nimesh/demoK8/DemoK8Application.java
@@ -0,0 +1,13 @@
+package com.nimesh.demoK8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoK8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoK8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimesh/demoK8/DemoK8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimesh.demoK8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoK8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
